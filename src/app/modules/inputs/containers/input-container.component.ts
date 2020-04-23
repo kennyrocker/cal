dfe334c6e85a9911ad467bdd918a32aa085e5d9d
kennyrocker <kennyrocker82@gmail.com>
2020-04-23 16:19:05 -0400

    Updated Input Styles

--- src/app/modules/inputs/containers/input-container.component.ts
+++ src/app/modules/inputs/containers/input-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CalData } from 'src/app/constants/interfaces/cal-data';
 import { InputGroup } from 'src/app/constants/enums/input-group';
 
@@ -6,20 +6,28 @@ import * as reducerRoot from '../../../reducers/index';
 import { Store } from '@ngrx/store';
 import { AddConstantIncomeItemAction,
   AddConstantExpenseItemAction, AddPeriodicalVariableItemAction } from 'src/app/actions/calData.action';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-input-container',
   templateUrl: './input-container.component.html',
   styleUrls: ['./input-container.component.scss']
 })
-export class InputContainerComponent  {
+export class InputContainerComponent implements OnInit {
 
   // tslint:disable-next-line:no-input-rename
   @Input('data')
   public data: CalData;
   public groupType = InputGroup;
+  private backUrl: string;
 
-  constructor(public store: Store<reducerRoot.CalDataState>) {
+  constructor(public store: Store<reducerRoot.CalDataState>,
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
+
+  public ngOnInit(): void {
+    this.backUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   public addConstantIncomeItem(): void {
@@ -37,6 +45,10 @@ export class InputContainerComponent  {
   // performance boost
   public trackByItem(index, item): any {
     return item ? item.id : undefined;
+  }
+
+  public routeBack(): void {
+    this.router.navigateByUrl(this.backUrl);
   }
 
 }
